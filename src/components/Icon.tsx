945e9c8f2746c78ea194407ace5ce32490d7c59d
NNQA <anhla12h@gmail.com>
2023-09-17 20:08:29 +0700

    refactor: refactor icon Component

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,12 +1,16 @@
-import { FC } from "react";
-import * as HIcons from "@heroicons/react/24/solid";
-const Icon: FC<{ icon: string }> = (props) => {
-  const { ...icons } = HIcons;
-  const TheIcon: React.ComponentType<React.SVGProps<SVGSVGElement>> = (
-    icons as any
-  )[props.icon];
+import * as SolidIcons from "@heroicons/react/24/solid";
+import * as OutlineIcons from "@heroicons/react/24/outline";
+export type iconName = keyof typeof SolidIcons | keyof typeof OutlineIcons;
+type HeroIconProps = {
+  solid?: false;
+  iconName: iconName;
+  className?: string;
+};
 
-  return <>{TheIcon && <TheIcon className="" />}</>;
+const Icon = ({ solid, iconName, className }: HeroIconProps): JSX.Element => {
+  const Icon = solid ? SolidIcons[iconName] : OutlineIcons[iconName];
+
+  return <Icon className={className}></Icon>;
 };
 
 export default Icon;
