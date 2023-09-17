49595c761a55ab85cf100feb41dcdf880ab627fb
NNQA <anhla12h@gmail.com>
2023-09-17 23:33:10 +0700

    refactor: refactor responsive for login page

--- src/route/login/FormLogin.tsx
+++ src/route/login/FormLogin.tsx
@@ -47,14 +47,11 @@ const FormLogin = () => {
   console.log(formik.values.email);
   return (
     <div
-      className="w-full sm:p-2 px-2 py-4 md:w-full flex flex-col  bg-white bg-opacity-40 rounded-lg shadow-md 
+      className="w-full sm:p-2 px-2 py-4 md:w-full flex flex-col sm:h-[80%] bg-white bg-opacity-40 rounded-lg shadow-md 
        justify-center gap-4 items-center "
     >
       <div className="text-center mb-5 flex flex-col gap-2">
-        <h2>Login your account</h2>
-        <p className="text-xs font-medium text-opacity-30 text-black">
-          It's free and easy
-        </p>
+        <h2 className="text-sm sm:text-2xl">Login your account</h2>
       </div>
       <form
         className="flex flex-col gap-1 w-2/3"
@@ -89,7 +86,7 @@ const FormLogin = () => {
           message={formik.errors.password}
         ></Input>
         <div className="mt-2 flex flex-col gap-4">
-          <div className="flex gap-2">
+          <div className="hidden sm:flex gap-2">
             <input type="checkbox" className="items-start " />
             <p className="text-xs font-bold text-fiord-cfg-900 text-opacity-60">
               Remember me
@@ -103,25 +100,25 @@ const FormLogin = () => {
           ></Button>
         </div>
       </form>
-      <div className=" w-2/3 gap-2 text-fiord-cfg-900 text-opacity-80  mt-2">
+      <div className=" w-2/3 gap-2 text-fiord-cfg-900 text-opacity-80  mt-2 hidden sm:block">
         <div className="flex justify-center text-center items-center gap-2">
           <hr className="h-[1px] w-[15%] bg-fiord-cfg-900 bg-opacity-50" />
-          <p className="text-[12px] font-bold">Or Sign in with</p>
+          <p className="text-[12px] font-bold sm:">Or Sign in with</p>
           <hr className="h-[1px] w-[15%] bg-fiord-cfg-900 bg-opacity-50" />
         </div>
       </div>
       <div className="flex gap-3 items-start">
         <div className="h-[2.2rem] w-fit text-fiord-cfg-500 font-medium px-2 gap-2 text-xs flex justify-center items-center  mt-1  border-solid	bg-transparent  rounded-lg shadow-lg shadow-fiord-cfg-500/40">
           <i className="uil uil-apple text-lg"></i>
-          <p className="font-bold">Apple</p>
+          <p className="font-bold hidden sm:flex">Apple</p>
         </div>
         <div className="h-[2.2rem] w-fit text-fiord-cfg-500 font-medium px-2 gap-2 text-xs flex justify-center items-center  mt-1  border-solid	bg-transparent  rounded-lg shadow-lg shadow-fiord-cfg-500/40">
           <i className="uil uil-facebook   text-lg"></i>
-          <p className="font-bold">Facebook</p>
+          <p className="font-bold hidden sm:flex">Facebook</p>
         </div>
         <div className="h-[2.2rem] w-fit text-fiord-cfg-500 font-medium px-2 gap-2 text-xs flex justify-center items-center  mt-1  border-solid	bg-transparent  rounded-lg shadow-lg shadow-fiord-cfg-500/40">
           <i className="uil uil-google  font-bold  text-lg"></i>
-          <p className="font-bold">Google</p>
+          <p className="font-bold hidden sm:flex">Google</p>
         </div>
       </div>
     </div>
