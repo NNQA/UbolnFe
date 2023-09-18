1e70e30aa3ffb2eee83ad8789189c724daa0cb63
NNQA <anhla12h@gmail.com>
2023-09-18 17:45:20 +0700

    add: additional fake data

--- src/lib/data/fake.ts
+++ src/lib/data/fake.ts
@@ -89,4 +89,130 @@ export const fakeData = [
       },
     ],
   },
+  {
+    serial: 3,
+    quoteStartYmd: "2023-09-11",
+    quoteEndYmd: "2023-09-17",
+    quotePriceDiscount: 30,
+    customer: {
+      customerId: "3",
+      name: "Nf",
+    },
+    customerOrders: [
+      {
+        quantity: 1,
+        product: {
+          productId: "1",
+        },
+      },
+    ],
+  },
+  {
+    serial: 4,
+    quoteStartYmd: "2023-09-12",
+    quoteEndYmd: "2023-09-18",
+    quotePriceDiscount: 55,
+    customer: {
+      customerId: "4",
+      name: "Mcill",
+    },
+    customerOrders: [
+      {
+        quantity: 4,
+        product: {
+          productId: "2",
+        },
+      },
+    ],
+  },
+  {
+    serial: 5,
+    quoteStartYmd: "2023-09-13",
+    quoteEndYmd: "2023-09-19",
+    quotePriceDiscount: 40,
+    customer: {
+      customerId: "5",
+      name: "Icd",
+    },
+    customerOrders: [
+      {
+        quantity: 2,
+        product: {
+          productId: "5",
+        },
+      },
+    ],
+  },
+  {
+    serial: 1,
+    quoteStartYmd: "2023-09-09",
+    quoteEndYmd: "2023-09-15",
+    quotePriceDiscount: 44,
+    customer: {
+      customerId: "1",
+      name: "Anh",
+    },
+    customerOrders: [
+      {
+        quantity: 2,
+        product: {
+          productId: "3",
+        },
+      },
+    ],
+  },
+  {
+    serial: 2,
+    quoteStartYmd: "2023-09-10",
+    quoteEndYmd: "2023-09-16",
+    quotePriceDiscount: 50,
+    customer: {
+      customerId: "2",
+      name: "Eminem",
+    },
+    customerOrders: [
+      {
+        quantity: 3,
+        product: {
+          productId: "4",
+        },
+      },
+    ],
+  },
+  {
+    serial: 1,
+    quoteStartYmd: "2023-09-09",
+    quoteEndYmd: "2023-09-15",
+    quotePriceDiscount: 44,
+    customer: {
+      customerId: "1",
+      name: "Anh",
+    },
+    customerOrders: [
+      {
+        quantity: 2,
+        product: {
+          productId: "3",
+        },
+      },
+    ],
+  },
+  {
+    serial: 2,
+    quoteStartYmd: "2023-09-10",
+    quoteEndYmd: "2023-09-16",
+    quotePriceDiscount: 50,
+    customer: {
+      customerId: "2",
+      name: "Eminem",
+    },
+    customerOrders: [
+      {
+        quantity: 3,
+        product: {
+          productId: "4",
+        },
+      },
+    ],
+  },
 ];
